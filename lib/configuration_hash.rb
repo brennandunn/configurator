57ba3227f6fc93095fc2129a028734fa892c848f
Brennan Dunn <brennandunn@brennan-dunns-macbook-pro-67.local>
2008-05-30 18:52:21 -0400

    Now with the option of setting configurations to classes, and not just instances

--- lib/configuration_hash.rb
+++ lib/configuration_hash.rb
@@ -5,13 +5,15 @@ class ConfigurationHash < ActiveRecord::Base
   
   class << self
     
-    def find_by_key_and_owner(key, owner, namespace = nil)
-      options = namespace ? { :namespace => namespace } : {}
-      find(:first, :conditions => { :key => key, :associated_id => owner.id, :associated_type => owner.class.name }.merge(options))
+    def find_by_key_and_owner(key, owner, call_type)
+      find(:first, :conditions => { :key => key, 
+                                    :associated_id => call_type == :instance ? owner.id : nil, 
+                                    :associated_type => call_type == :instance ? owner.class.name : owner.name })
     end
     
-    def find_all_by_owner(owner)
-      find(:all, :conditions => { :associated_id => owner.id, :associated_type => owner.class.name })
+    def find_all_by_owner(owner, call_type)
+      find(:all, :conditions => { :associated_id => call_type == :instance ? owner.id : nil, 
+                                  :associated_type => call_type == :instance ? owner.class.name : owner.name })
     end
     
   end
--- lib/configurator.rb
+++ lib/configurator.rb
@@ -7,7 +7,16 @@ module Configurator
     end
     
     def get_default_configuration
-      @@default_configuration rescue {}
+      @@default_configuration
+    end
+    
+    def config
+      @config_proxy ||= ConfigProxy.new(:class, self)
+    end
+    
+    def config=(hsh)
+      config.from_hash(hsh)
+      config
     end
   
   end
@@ -15,90 +24,68 @@ module Configurator
   module InstanceMethods
     
     def config
-      @config_proxy ||= ConfigProxy.new(self)
+      @config_proxy ||= ConfigProxy.new(:instance, self)
     end
 
     def config=(hsh)
       config.from_hash(hsh)
       config
     end
+    
+  end
+  
+  class ConfigProxy
 
-    class ConfigProxy
+    attr_reader :defaults
 
-      attr_reader :defaults
-
-      def initialize(reference)
-        @reference = reference
+    def initialize(call_type, reference)
+      @reference, @call_type = reference, call_type
+      case call_type
+      when :instance
         @options = { :associated_id => reference.id, :associated_type => reference.class.name }
         @defaults = reference.class.get_default_configuration || {}
+      when :class
+        @options = { :associated_type => reference.name }
+        @defaults = reference.get_default_configuration || {}
       end
-
-      def [](*keys)
-        namespace, key = [keys].flatten
-        if key.nil?
-          key = namespace
-          namespace = nil
-        end
-
-        pair = ConfigurationHash.find_by_key_and_owner(key.to_s, @reference, namespace ? namespace.to_s : nil)
-        if pair.nil?
-          namespace ? @defaults[namespace][key] : @defaults[key] rescue nil
-          #@defaults[namespace ? keys : key.to_sym] || nil
-        else
-          pair.value
-        end
-        
-      end
-
-      def []=(*keys)
-        if keys.size == 3
-          namespace, key = keys[0], keys[1]
-          value = keys[2]
-        else
-          key, value = keys[0], keys[1]
-        end
-                
-        pair = ConfigurationHash.find_by_key_and_owner(key.to_s, @reference, namespace ? namespace.to_s : nil)
-        unless pair
-          pair = ConfigurationHash.new(@options)
-          pair.key, pair.value = key.to_s, value
-          pair.namespace = namespace.to_s if namespace
-          pair.save
-        else
-          pair.value = value
-          pair.save
-        end
-        value
-      end
-      
-      def namespace(ns)
-        configs = ConfigurationHash.find_all_by_namespace(ns.to_s)
-        configs.inject({}) { |hsh, c| hsh[c.key.intern] = c.value; hsh }
-      end
-
-      def to_hash(with_defaults = false)
-        Hash[ *ConfigurationHash.find_all_by_owner(@reference).map { |pair| [pair.key, pair.value] }.flatten ]
-      end
-
-      def from_hash(hsh)
-        hsh.each do |key, value|
-          if value.is_a?(Hash)
-            namespace = key
-            value.each do |key, value|
-              self[namespace, key] = value
-            end
-          else
-            self[key] = value
-          end
-        end
-      end
-
-      def method_missing(method, *args, &block)
-        self[method.to_s]
-      end
-
     end
-    
+
+    def [](key)
+      pair = ConfigurationHash.find_by_key_and_owner(key.to_s, @reference, @call_type)
+      if pair.nil?
+        @defaults[key.to_sym] || nil
+      else
+        pair.value
+      end
+    end
+
+    def []=(key, value)
+      pair = ConfigurationHash.find_by_key_and_owner(key.to_s, @reference, @call_type)
+      unless pair
+        pair = ConfigurationHash.new(@options)
+        pair.key, pair.value = key.to_s, value
+        pair.save
+      else
+        pair.value = value
+        pair.save
+      end
+      value
+    end
+
+    def to_hash(with_defaults = false)
+      Hash[ *ConfigurationHash.find_all_by_owner(@reference).map { |pair| [pair.key, pair.value] }.flatten ]
+    end
+
+    def from_hash(hsh)
+      hsh.each do |key, value|
+        self[key] = value
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      self[method.to_s]
+    end
+
   end
   
   def self.included(receiver)

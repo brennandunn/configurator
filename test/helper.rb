a82d50cd2345143635be666ec13144e40a6f50aa
Andrew Newman <andrew.newman@sdx.com.au>
2012-08-08 11:51:20 +1000

    Tests running

--- test/helper.rb
+++ test/helper.rb
@@ -2,8 +2,39 @@ require 'rubygems'
 require 'active_record'
 require 'test/unit'
 require 'configurator'
+require 'shoulda'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:", )
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+  ::ActiveRecord::Base.class_eval do
+    silence do
+      connection.create_table :config, :force => true do |t|
+        t.references :associated, :polymorphic => true
+        t.string :namespace
+        t.string :key, :limit => 40, :null => false
+        t.string :value
+        t.string :data_type, :limit => 40
+      end
+
+      connection.create_table :users, :force => true do |t|
+        t.string :handle
+      end
+
+      connection.create_table :companies, :force => true do |t|
+        t.string :name
+      end
+    end
+  end
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
+  end
+end
+
+setup_db
 
 class User < ActiveRecord::Base
   include Configurator
@@ -11,34 +42,6 @@ end
 
 class Company < ActiveRecord::Base
   include Configurator
-  
-  default_configuration :notify_users? => true, :salary => { :default_for_manager => '$55,000', :default_for_employee => '$25,000' }
-end
 
-def setup_db
-  ::ActiveRecord::Base.class_eval do
-    silence do
-      connection.create_table :config, :force => true do |t|
-        t.references    :associated, :polymorphic => true
-        t.string        :namespace
-        t.string        :key,         :limit => 40,     :null => false
-        t.string        :value
-        t.string        :data_type,   :limit => 40
-      end
-    
-      connection.create_table :users, :force => true do |t|
-        t.string        :handle
-      end
-    
-      connection.create_table :companies, :force => true do |t|
-        t.string        :name
-      end
-    end
-  end
-end
- 
-def teardown_db
-  ActiveRecord::Base.connection.tables.each do |table|
-    ActiveRecord::Base.connection.drop_table(table)
-  end
+  default_configuration :notify_users? => true, :salary => { :default_for_manager => '$55,000', :default_for_employee => '$25,000' }
 end
